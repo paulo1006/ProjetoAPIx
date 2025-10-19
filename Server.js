1eb0d7f71e2a5a958d5f726ed3139e3b58d9c678
Leandrojr17 <leandronepomuceno1996@gmail.com>
2025-10-19 17:24:37 -0300

    Implementado a criação do usuário e o Login

--- Server.js
+++ Server.js
@@ -1,12 +1,72 @@
+import 'dotenv/config'
 import express from 'express'
 import pkg from '@prisma/client'
-const { PrismaClient } = pkg
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
 
+const { PrismaClient } = pkg
 const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-app.post('/estoques', async (req, res) => {
+app.post('/usuarios', async(req,res)=>{
+  try{
+    const{nome, email, senha} = req.body
+    
+    const senhaHash = await bcrypt.hash(senha,10)
+    
+    const novoUsuario = await prisma.usuario.create({
+      data: {nome,email,senha: senhaHash}
+    })
+
+    res.status(201).json({ message: 'Usuário cadastrado com sucesso!!', usuario: novoUsuario})
+  
+  } catch(error){
+
+    res.status(500).json({ error: 'Erro ao cadastrar novo usuário!!'})
+
+  }
+})
+
+app.post('/login', async(req,res) => {
+    try{
+      const{ email, senha} = req.body
+      
+      const usuario = await prisma.usuario.findUnique ({where: {email} })
+      if(!usuario) return res.status(404).json({ error: 'Usuário não encontrado!!'})
+
+      const senhaValida = await bcrypt.compare(senha, usuario.senha)
+      if(!senhaValida) return res.status(401).json({ error: 'Senha Incorreta!!'})
+
+
+      const token = jwt.sign(
+        {id: usuario.id, nome: usuario.nome, email: usuario.email},
+        process.env.JWT_SECRET,
+        {expiresIn: process.env.JWT_EXPIRES_IN }
+      )
+
+      res.status(200).json({ message: 'Login bem sucedido!!', token})
+
+    } catch(error){
+      res.status(500).json({ error: 'Erro ao fazer o login'})
+    }
+})
+
+function autenticarToken(req,res,next){
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if(!token) return res.status(401).json({ error: 'Token ausente'})
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, usuario) => {
+    if(err) return res.status(403).json({error: 'Token inválido ou Token expirado!!'})
+    req.usuario = usuario
+    next()
+  })
+}
+
+
+app.post('/estoques',autenticarToken ,async (req, res) => {
   const estoque = await prisma.estoque.create({
     data: {
       nome: req.body.nome,
@@ -16,7 +76,7 @@ app.post('/estoques', async (req, res) => {
   res.status(201).json(estoque)
 })
 
-app.get('/estoques', async (req, res) => {
+app.get('/estoques',autenticarToken, async (req, res) => {
   const filtros = {}
   if (req.query.nome) filtros.nome = req.query.nome
   if (req.query.centro) filtros.centro = req.query.centro
@@ -28,7 +88,7 @@ app.get('/estoques', async (req, res) => {
   res.status(200).json(estoques)
 })
 
-app.get('/estoques/:id', async (req, res) => {
+app.get('/estoques/:id', autenticarToken,async (req, res) => {
   const estoque = await prisma.estoque.findUnique({
     where: { id: req.params.id },
     include: { produtos: true }
@@ -39,7 +99,7 @@ app.get('/estoques/:id', async (req, res) => {
   res.status(200).json(estoque)
 })
 
-app.put('/estoques/:id', async (req, res) => {
+app.put('/estoques/:id', autenticarToken,async (req, res) => {
   const estoque = await prisma.estoque.update({
     where: { id: req.params.id },
     data: {
@@ -50,13 +110,13 @@ app.put('/estoques/:id', async (req, res) => {
   res.status(200).json(estoque)
 })
 
-app.delete('/estoques/:id', async (req, res) => {
+app.delete('/estoques/:id', autenticarToken,async (req, res) => {
   await prisma.estoque.delete({ where: { id: req.params.id } })
   res.status(200).json({ message: 'Estoque deletado com sucesso!' })
 })
 
 
-app.post('/produtos', async (req, res) => {
+app.post('/produtos',autenticarToken, async (req, res) => {
   const produto = await prisma.produto.create({
     data: {
       nome: req.body.nome,
@@ -68,7 +128,7 @@ app.post('/produtos', async (req, res) => {
   res.status(201).json(produto)
 })
 
-app.get('/produtos', async (req, res) => {
+app.get('/produtos',autenticarToken, async (req, res) => {
   const filtros = {}
   if (req.query.nome) filtros.nome = req.query.nome
   if (req.query.estoqueId) filtros.estoqueId = req.query.estoqueId
@@ -80,7 +140,7 @@ app.get('/produtos', async (req, res) => {
   res.status(200).json(produtos)
 })
 
-app.get('/produtos/:id', async (req, res) => {
+app.get('/produtos/:id',autenticarToken, async (req, res) => {
   const produto = await prisma.produto.findUnique({
     where: { id: req.params.id },
     include: { estoque: true }
@@ -91,7 +151,7 @@ app.get('/produtos/:id', async (req, res) => {
   res.status(200).json(produto)
 })
 
-app.put('/produtos/:id', async (req, res) => {
+app.put('/produtos/:id',autenticarToken,async (req, res) => {
   const produto = await prisma.produto.update({
     where: { id: req.params.id },
     data: {
@@ -104,7 +164,7 @@ app.put('/produtos/:id', async (req, res) => {
   res.status(200).json(produto)
 })
 
-app.delete('/produtos/:id', async (req, res) => {
+app.delete('/produtos/:id',autenticarToken,async (req, res) => {
   await prisma.produto.delete({ where: { id: req.params.id } })
   res.status(200).json({ message: 'Produto deletado com sucesso!' })
 })
